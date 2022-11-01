b21c201fa1776014bb28e5a032375ec759428349
albarabr <farrazalbar03@gmail.com>
2022-11-01 10:07:02 +0700

    tugas pert 7

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+     <h1><?php echo $judul ?></h1>
+     <h4>Nama</h4>
+     <ul type="disc">
+          <li>Nama Depan : Mohamad</li>
+          <li>Nama Belakang : Ali</li>
+     </ul>
+     <br>
+     <h4>Alamat</h4>
+     <ul type="none">
+          <li> Dukuh Wungu</li>
+     </ul>
+
+     <h4>Tempat Lahir</h4>
+     <ul type="none">
+          <li>Bandung</li>
+     </ul>
+
+     <h4>Olah Raga Favorit</h4>
+     <ul type="square">
+          <li>Bulutangkis</li>
+          <li>Basket</li>
+     </ul>
+</section>
